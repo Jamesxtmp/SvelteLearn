203ce5bd882a79e34c5e62d085cba15f2589d2e0
Jamesxtmp <gen22xtmp@gmail.com>
2022-11-02 14:20:49 -0600

    Inicialized

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<ul>
+    <li><h3><a href="/">Home</a></h3></li>
+    <li><h3><a href="/about">About</a></h3></li>
+</ul>
+<slot></slot>
+
+<style>
+    ul{
+        list-style: none;
+        display: flex;
+    }
+    li{
+        padding: 0 8px;
+    }
+    a{
+        text-decoration: none;
+        color: rgb(0, 0, 0);
+        text-decoration: overline;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Home</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint labore molestiae soluta debitis possimus iure. Expedita, voluptatum quam? Doloribus excepturi ipsa et! Libero ea quasi, consequatur culpa rem aperiam sed velit consectetur excepturi, dicta molestiae quia sint nesciunt alias cumque provident odit? Iure enim eligendi esse consectetur ipsum voluptas aperiam nostrum culpa? Nobis in qui dolor necessitatibus iste et esse.</p>
+
+<style>
+    h1{
+        text-align: center;
+    }
+    p{
+        width: 80%;
+        margin: auto;
+        text-align: justify;
+    }
+</style>
